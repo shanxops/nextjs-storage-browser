0f76b9d55d116c5ffac8e80dee7de752b8bb30c4
Scott R <6165315+reesscot@users.noreply.github.com>
2024-11-06 15:42:34 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,34 +4,16 @@ import { Amplify } from "aws-amplify";
 import { signOut } from "aws-amplify/auth";
 
 import { Button, withAuthenticator } from "@aws-amplify/ui-react";
-import {
-  createStorageBrowser,
-  createAmplifyAuthAdapter,
-  elementsDefault,
-} from "@aws-amplify/ui-react-storage/browser";
+
 import "@aws-amplify/ui-react-storage/styles.css";
 import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
 
 import config from "../amplify_outputs.json";
+import { StorageBrowser } from "@aws-amplify/ui-react-storage";
 
 Amplify.configure(config);
 
 function Example() {
-  const { StorageBrowser } = createStorageBrowser({
-    elements: elementsDefault,
-    config: createAmplifyAuthAdapter({
-      options: {
-        defaultPrefixes: [
-          "media-readwritedelete/",
-          "media-readonly/",
-          "shared-folder-readwrite/",
-          (identityId: string) => `protected-useronlyreadwritedelete/${identityId}/`,
-          (identityId: string) => `private-useronlyreadwritedelete/${identityId}/`,
-        ],
-      },
-    }),
-  });
-
   return (
     <>
       <Button
