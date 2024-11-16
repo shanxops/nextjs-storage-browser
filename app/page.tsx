992c02373209dd28e88cb0b43e2eb9b0bac52c27
Scott R <6165315+reesscot@users.noreply.github.com>
2024-11-15 17:00:24 -0700

    chore: add version to demo app

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Amplify } from "aws-amplify";
 import { signOut } from "aws-amplify/auth";
 
-import { Button, withAuthenticator } from "@aws-amplify/ui-react";
+import { Button, withAuthenticator, Text, View, Flex } from "@aws-amplify/ui-react";
 
 import "@aws-amplify/ui-react-storage/styles.css";
 import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
@@ -11,13 +11,24 @@ import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
 import config from "../amplify_outputs.json";
 import { StorageBrowser } from "@aws-amplify/ui-react-storage";
 
+import { VERSION as AMPLIFY_VERSION } from './version-aws-amplify'
+import { VERSION as AMPLIFY_UI_VERSION } from './version-aws-amplify-ui-react-storage'
+
+
 Amplify.configure(config);
 
 function Example() {
   return (
-    <>
-      <Button
+    <Flex
+      direction="column"
+      width="100vw"
+      height="100vh"
+      overflow="hidden"
+      padding="xl"
+    >
+      <Flex>      <Button
         marginBlockEnd="xl"
+        alignSelf="flex-start"
         size="small"
         onClick={() => {
           signOut();
@@ -25,8 +36,14 @@ function Example() {
       >
         Sign Out
       </Button>
+        <Text>aws-amplify: {AMPLIFY_VERSION}</Text>
+        <Text>ui-react-storage: {AMPLIFY_UI_VERSION}</Text></Flex>
+
+      <View flex="1" overflow="hidden">
+
       <StorageBrowser />
-    </>
+      </View>
+    </Flex>
   );
 }
 
